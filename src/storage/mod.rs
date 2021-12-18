d7958fd88e1e968579f6ea415248bf6bbc00e881
Rishav Bhowmik <bhowmikrishav@gmail.com>
2021-12-19 01:31:38 +0530

    # Storage::open implemented - # StorageHeader::from_bytes - # BlockHeader::from_bytes - Storage params docs for variables - end_block_count fix in Storage.write_block # Storage.delete_block fix - check block exists - no overwriting deleted block without hard_delete

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -15,6 +15,10 @@ impl StorageHeader {
     fn new(block_len: u32) -> Self {
         StorageHeader { block_len }
     }
+    fn from_bytes(bytes: &[u8; STORAGE_HEADER_SIZE]) -> StorageHeader {
+        let block_len = bytes_to_u32(bytes);
+        StorageHeader { block_len }
+    }
     fn to_bytes(&self) -> [u8; STORAGE_HEADER_SIZE] {
         u32_to_bytes(self.block_len)
     }
@@ -34,6 +38,12 @@ impl BlockHeader {
             block_data_size: block_data_size,
         }
     }
+    fn from_bytes(bytes: &[u8; BLOCK_HEADER_SIZE]) -> BlockHeader {
+        let block_data_size = bytes_to_u32(bytes);
+        BlockHeader {
+            block_data_size: block_data_size,
+        }
+    }
     fn to_bytes(&self) -> [u8; BLOCK_HEADER_SIZE] {
         u32_to_bytes(self.block_data_size)
     }
@@ -44,11 +54,17 @@ use std::fs::{File, OpenOptions};
 
 pub struct Storage {
     header: StorageHeader,
+    /// Map of empty blocks in the storage file
     free_blocks: BTreeSet<u32>,
+    /// Number of blocks in the storage file (used or free)
     end_block_count: u32,
+    /// File object for writing
     file_writer: File,
+    /// Index of last written byte in the file
     write_pointer: u64,
+    /// File object for reading
     file_reader: File,
+    /// Index of last read byte in the file
     read_pointer: u64,
 }
 
@@ -103,17 +119,11 @@ impl Storage {
 
         let mut storage = Storage {
             header: StorageHeader::new(block_len as u32),
-            /// Map of empty blocks in the storage file
             free_blocks: BTreeSet::new(),
-            /// Number of blocks in the storage file (used or free)
             end_block_count: 0,
-            /// File object for writing
             file_writer,
-            /// Index of last written byte in the file
             write_pointer,
-            /// File object for reading
             file_reader,
-            /// Index of last read byte in the file
             read_pointer,
         };
         if storage.set_storage_header().is_err() {
@@ -127,19 +137,44 @@ impl Storage {
 
     // TODO: Implement
     /// Open existing storage file
-    // pub fn open(file_path: String) -> Result<Storage, Error> {
-    // let file_writer = Storage::open_file_writer(&file_path, false);
-    // if file_writer.is_err() {
-    //     return Err(file_writer.unwrap_err());
-    // }
-    // let (file_writer, write_pointer) = file_writer.unwrap();
+    pub fn open(file_path: String) -> Result<Storage, Error> {
+        let file_writer = Storage::open_file_writer(&file_path, false);
+        if file_writer.is_err() {
+            return Err(file_writer.unwrap_err());
+        }
+        let (file_writer, write_pointer) = file_writer.unwrap();
+        let file_reader = Storage::open_file_reader(&file_path);
+        if file_reader.is_err() {
+            return Err(file_reader.unwrap_err());
+        }
+        let (file_reader, read_pointer) = file_reader.unwrap();
 
-    // let file_reader = Storage::open_file_reader(&file_path);
-    // if file_reader.is_err() {
-    //     return Err(file_reader.unwrap_err());
-    // }
-    // let (file_reader, read_pointer) = file_reader.unwrap();
-    // }
+        // - init storage object
+        let mut storage = Storage {
+            header: StorageHeader::new(0),
+            free_blocks: BTreeSet::new(),
+            end_block_count: 0,
+            file_writer,
+            write_pointer,
+            file_reader,
+            read_pointer,
+        };
+        // - read and update storage header from file
+        if storage.get_storage_header().is_err() {
+            return Err(Error {
+                code: 2,
+                message: "Could not init storage".to_string(),
+            });
+        }
+        // - read file and count
+        // -- total blocks - update self.end_block_count
+        // -- free blocks - update self.free_blocks
+        let blocks_status_result = storage.read_storage_block_headers();
+        if blocks_status_result.is_err() {
+            return Err(blocks_status_result.unwrap_err());
+        }
+        Ok(storage)
+    }
 
     // # File IO Functions
 
@@ -178,6 +213,135 @@ impl Storage {
         }
         self.write_pointer += write_size as u64;
         Ok(write_size)
+    }
+    /// Get storage header from storage file
+    /// - Read storage header from file
+    /// - update storage header in object
+    fn get_storage_header(&mut self) -> Result<usize, Error> {
+        use std::io::prelude::*;
+        let file = &mut self.file_reader;
+        // - Read storage header from file
+        // -- seek reader pointer to beginning of file
+        let ptr_seek_result = file.seek(std::io::SeekFrom::Start(0));
+        if ptr_seek_result.is_err() {
+            return Err(Error {
+                code: 3,
+                message: "Could not seek file pointer".to_string(),
+            });
+        }
+        // -- read storage header
+        let mut header_bytes = [0u8; STORAGE_HEADER_SIZE];
+        self.read_pointer = ptr_seek_result.unwrap();
+        let read_result = file.read(&mut header_bytes);
+        if read_result.is_err() {
+            return Err(Error {
+                code: 2,
+                message: "Could not read from file".to_string(),
+            });
+        }
+        // -- verify read operation was successful
+        let read_size = read_result.unwrap();
+        if read_size != STORAGE_HEADER_SIZE as usize {
+            return Err(Error {
+                code: 2,
+                message: "Could not read all header bytes from file".to_string(),
+            });
+        }
+        // -- update read pointer
+        self.read_pointer += read_size as u64;
+        // - parse storage header
+        let storage_header = StorageHeader::from_bytes(&header_bytes);
+        // - copy storage header to storage object
+        self.header = storage_header;
+        // - return read pointer
+        Ok(read_size)
+    }
+    /// Count number of blocks in storage file
+    /// -- total blocks - update self.end_block_count
+    /// -- free blocks - update self.free_blocks
+    fn read_storage_block_headers(&mut self) -> Result<usize, Error> {
+        use std::io::prelude::*;
+        let file = &mut self.file_reader;
+        // - seek reader pointer to end of file
+        let ptr_seek_result = file.seek(std::io::SeekFrom::Start(0));
+        if ptr_seek_result.is_err() {
+            return Err(Error {
+                code: 3,
+                message: "Could not seek file pointer".to_string(),
+            });
+        }
+        // - update read pointer
+        self.read_pointer = ptr_seek_result.unwrap();
+        // - read file and count
+        // -- total blocks - update self.end_block_count
+        // -- free blocks - update self.free_blocks
+        let mut free_blocks = BTreeSet::new();
+        // -- seek reader pointer to end of STORAGE_HEADER_SIZE
+        let ptr_seek_result = file.seek(std::io::SeekFrom::Start(STORAGE_HEADER_SIZE as u64));
+        if ptr_seek_result.is_err() {
+            return Err(Error {
+                code: 3,
+                message: "Could not seek file pointer".to_string(),
+            });
+        }
+        // -- traverse all blocks in file, untill end of file
+        let mut block_index = 0;
+        loop {
+            // - read block header
+            let mut block_header_bytes = [0u8; BLOCK_HEADER_SIZE];
+            let read_result = file.read(&mut block_header_bytes);
+            if read_result.is_err() {
+                return Err(Error {
+                    code: 2,
+                    message: "Could not read from file".to_string(),
+                });
+            }
+            // -- check end of file
+            // -- verify read operation was successful
+            let read_size = read_result.unwrap();
+            if read_size == 0 {
+                // end of file reached
+                break;
+            }
+            if read_size != BLOCK_HEADER_SIZE as usize {
+                return Err(Error {
+                    code: 2,
+                    message: "Could not read all header bytes from file".to_string(),
+                });
+            }
+            // -- update read pointer
+            self.read_pointer += read_size as u64;
+            // -- parse block header
+            let block_header = BlockHeader::from_bytes(&block_header_bytes);
+            // - check if block is free
+            if block_header.block_data_size == 0 {
+                // -- add block to free blocks
+                free_blocks.insert(block_index);
+            }
+            // -- increment block index
+            block_index += 1;
+            // - seek reader pointer to end of block
+            let ptr_seek_result = file.seek(std::io::SeekFrom::Current(self.header.block_len as i64));
+            if ptr_seek_result.is_err() {
+                return Err(Error {
+                    code: 3,
+                    message: "Could not seek file pointer".to_string(),
+                });
+            }
+            let ptr_seek_result = ptr_seek_result.unwrap();
+            self.read_pointer = ptr_seek_result;
+            // -- verify seek operation was successful
+            if ptr_seek_result != self.read_pointer {
+                // end of file reached
+                break;
+            }
+        }
+        // - update end block count
+        self.end_block_count = block_index;
+        // - update free blocks
+        self.free_blocks = free_blocks;
+        // - return
+        Ok(self.read_pointer as usize)
     }
     /// check if block is within storage file, without reading it from file (in memory)
     fn block_exists(&mut self, block_index: u32) -> bool {
@@ -313,12 +477,18 @@ impl Storage {
         self.free_blocks.remove(&block_index);
         // - update max_block_index
         if block_index >= self.end_block_count {
-            self.end_block_count = block_index;
+            self.end_block_count = block_index + 1;
         }
         // return write pointer
         Ok(self.write_pointer as usize)
     }
     pub fn delete_block(&mut self, block_index: usize, hard_delete: bool) -> Result<usize, Error> {
+        let block_index = block_index as u32;
+        if !self.block_exists(block_index as u32) {
+            return Ok(self.write_pointer as usize);
+        } else if hard_delete == false && self.free_blocks.contains(&block_index) {
+            return Ok(self.write_pointer as usize);
+        }
         use std::io::prelude::*;
         let block_length = self.header.block_len;
         let block_offset = STORAGE_HEADER_SIZE as usize
@@ -384,7 +554,6 @@ impl Storage {
             self.write_pointer += write_size as u64;
         }
         // update free_blocks map
-        let block_index = block_index as u32;
         self.free_blocks.insert(block_index);
         // return write pointer
         Ok(self.write_pointer as usize)
